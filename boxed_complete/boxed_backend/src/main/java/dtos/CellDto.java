e5c34d4bff7c47c9f1616b59a3dee2c7c306c47c
JasperVB3 <jasper.vandenbossche@hormail.com>
2019-02-07 09:30:15 +0100

    DTO's toegevoegd

---
+++ boxed_complete/boxed_backend/src/main/java/dtos/CellDto.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dtos;
+
+
+import java.util.List;
+
+/**
+ *
+ * @author JVDBG19
+ */
+public class CellDto {
+    private Integer cellId;
+    private String cellBlock;
+    private Integer size;
+    private boolean isolationCell;
+    private List<PrisonerDto> prisonerList;
+
+    public CellDto(Integer cellId, String cellBlock, Integer size, boolean isolationCell, List<PrisonerDto> prisonerList) {
+        this.cellId = cellId;
+        this.cellBlock = cellBlock;
+        this.size = size;
+        this.isolationCell = isolationCell;
+        this.prisonerList = prisonerList;
+    }
+
+    public Integer getCellId() {
+        return cellId;
+    }
+
+    public void setCellId(Integer cellId) {
+        this.cellId = cellId;
+    }
+
+    public String getCellBlock() {
+        return cellBlock;
+    }
+
+    public void setCellBlock(String cellBlock) {
+        this.cellBlock = cellBlock;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    public boolean isIsolationCell() {
+        return isolationCell;
+    }
+
+    public void setIsolationCell(boolean isolationCell) {
+        this.isolationCell = isolationCell;
+    }
+
+    public List<PrisonerDto> getPrisonerList() {
+        return prisonerList;
+    }
+
+    public void setPrisonerList(List<PrisonerDto> prisonerList) {
+        this.prisonerList = prisonerList;
+    }
+
+    
+}
