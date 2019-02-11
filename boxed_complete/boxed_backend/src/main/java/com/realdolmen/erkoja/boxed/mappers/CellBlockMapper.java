c0ad06a55b5cbb21efe66ef9c7224a3bc5775b72
Kobe <Kobe@DESKTOP-M9V8K8K>
2019-02-11 15:27:42 +0100

    mappers corrected

--- boxed_complete/boxed_backend/src/main/java/com/realdolmen/erkoja/boxed/mappers/CellBlockMapper.java
+++ boxed_complete/boxed_backend/src/main/java/com/realdolmen/erkoja/boxed/mappers/CellBlockMapper.java
@@ -1,9 +1,11 @@
 
 package com.realdolmen.erkoja.boxed.mappers;
 
+import com.realdolmen.erkoja.boxed.domain.Cell;
 import com.realdolmen.erkoja.boxed.domain.CellBlock;
 import com.realdolmen.erkoja.boxed.domain.Guard;
 import com.realdolmen.erkoja.boxed.dtos.CellBlockDto;
+import com.realdolmen.erkoja.boxed.dtos.CellDto;
 import com.realdolmen.erkoja.boxed.dtos.GuardDto;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +25,15 @@ public class CellBlockMapper implements Function<CellBlock,CellBlockDto> {
             }
             dto.setGuards(guardDtoList);
         }
+        if(cellBlock.getCells() != null){
+             CellMapper cm = new CellMapper();
+             List<Cell> cells = cellBlock.getCells();
+             List<CellDto> cellDtos = new ArrayList<>();
+             for(Cell c : cells){
+                 cellDtos.add(cm.apply(c));
+             }
+             dto.setCells(cellDtos);
+            }
         return dto;
     }
 } 
